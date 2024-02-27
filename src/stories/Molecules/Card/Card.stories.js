db85f3893ef2e6fbbc47e1eadaaf84d9056f4bab
jsainz <jsainz@veridas.com>
2024-02-27 10:13:44 +0100

    Corregido el funcionamiento de cambio de valor de varias propiedades

--- src/stories/Molecules/Card/Card.stories.js
+++ src/stories/Molecules/Card/Card.stories.js
@@ -2,7 +2,7 @@ import { Card } from './Card';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
-  title: 'Components/Molecules/Card',
+  title: 'Components/Organisms/Card',
   component: Card,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
